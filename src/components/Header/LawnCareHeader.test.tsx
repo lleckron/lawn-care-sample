d4fd65de5c0ec48264523bc85c6636a4859599ee
lleckron <leckmanbaseball@gmail.com>
2023-06-05 13:33:17 -0400

    prelim tests for routed LawnCareHeader component

--- src/components/Header/LawnCareHeader.test.tsx
+++ src/components/Header/LawnCareHeader.test.tsx
@@ -1,14 +1,47 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
-import LawnCareHeader from './LawnCareHeader';
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import LawnCareHeader from './LawnCareHeader'
+import { MemoryRouter } from 'react-router-dom'
 
 describe('<LawnCareHeader />', () => {
-  test('it should mount', () => {
-    render(<LawnCareHeader />);
-    
-    const lawnCareHeader = screen.getByTestId('LawnCareHeader');
 
-    expect(lawnCareHeader).toBeInTheDocument();
-  });
-});
+	function renderLawnCareHeader() {
+		const utils = render(
+		  	<MemoryRouter>
+				<LawnCareHeader setSelectedService={jest.fn()} />
+		  	</MemoryRouter>
+		)
+	  
+		return {
+		  ...utils,
+		  	lawnCareHeader: screen.getByTestId('lawn-care-header'),
+		  	companyName: screen.getByText('Sample Lawn Care'),
+			companyPhone: screen.getByText('(765)-123-4567'),
+			companyEmail: screen.getByText('samplebusiness@email.com'),
+			navHomeLink: screen.getByText('Home'),
+			navAboutUsLink: screen.getByText('About Us'),
+			navServicesLink: screen.getByText('Services')
+		}
+	}
+
+  	test('it should mount', () => {
+		const { lawnCareHeader } = renderLawnCareHeader()
+  	  	expect(lawnCareHeader).toBeInTheDocument()
+  	})
+
+	test('it renders the Company name, phone number, and email', () => {
+		const { companyName, companyPhone, companyEmail } = renderLawnCareHeader()
+
+		expect(companyName).toBeInTheDocument()
+		expect(companyPhone).toBeInTheDocument()
+		expect(companyEmail).toBeInTheDocument()
+	})
+
+	test('it renders the clickable nav links', () => {
+		const { navHomeLink, navAboutUsLink, navServicesLink } = renderLawnCareHeader()
+
+		expect(navHomeLink).toBeInTheDocument()
+		expect(navAboutUsLink).toBeInTheDocument()
+		expect(navServicesLink).toBeInTheDocument()
+	})
+})
